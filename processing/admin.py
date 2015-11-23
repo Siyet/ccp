c711a609fb5cec0902968352709c4659a4d965ba
arseniy <cloud1567@yahoo.com>
2015-11-23 12:18:20 +0300

    запретил удаление обычным смертным

--- processing/admin.py
+++ processing/admin.py
@@ -4,4 +4,10 @@ from .models import ComposingSource
 class SourceAdmin(admin.ModelAdmin):
     list_display = ['type', 'file']
 
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return []
+        else:
+            return ['type']
+
 admin.site.register(ComposingSource, SourceAdmin)
