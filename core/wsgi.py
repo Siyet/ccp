69c7b1e64eb7ed5bea9e8a7475ca000086f70344
arseniy <cloud1567@yahoo.com>
2016-01-12 23:14:21 +0700

    wsgi configuration

--- core/wsgi.py
+++ core/wsgi.py
@@ -2,12 +2,9 @@ import os
 import sys
 import site
 
-sys.path.append('/var/www/cloud/data/www/network.itex.su')
+sys.path.append('/var/www/costumecode')
 
-site.addsitedir('/var/virtualenvs/itex_network/lib/python2.7/site-packages')
-
-activate_this = os.path.expanduser("/var/virtualenvs/itex_network/bin/activate_this.py")
-execfile(activate_this, dict(__file__=activate_this))
+site.addsitedir('/var/virtualenvs/costumecode/lib/python2.7/site-packages')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.prod")
 
