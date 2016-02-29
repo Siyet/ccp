7fda4e8b334cb85f50e4d72a83f43754cd04ee39
arseniy <cloud1567@yahoo.com>
2016-02-29 21:07:34 +0300

    removed odd `tools` folder

--- api/urls.py
+++ api/urls.py
@@ -1,39 +1,34 @@
 from django.conf.urls import url, include
-from api.views import CollectionsListView, ShirtInfoListView, SizeOptionsList, SizesList
-from api.views import CollectionFabricDesignsList, CollectionFabricColorsList, CollectionFabricsList
-from api.views import CollarTypeList, CuffTypeList, HemTypeList
-from api.views import HemTypeList, BackTypeList, PocketTypeList, PlacketTypeList, SleeveTypeList
-from api.views import TemplateShirtsList, TemplateShirtDetails
-
+import views
 
 shirt_urls = [
-    url(r'^showcase/$', TemplateShirtsList.as_view()),
-    url(r'^showcase/(?P<pk>[0-9]+)/$', TemplateShirtDetails.as_view(), name='templateshirt-detail'),
+    url(r'^showcase/$', views.TemplateShirtsList.as_view()),
+    url(r'^showcase/(?P<pk>[0-9]+)/$', views.TemplateShirtDetails.as_view(), name='templateshirt-detail'),
 ]
 
 collection_urls = [
-    url(r'^fabric/$', CollectionFabricsList.as_view()),
-    url(r'^fabric/color/$', CollectionFabricColorsList.as_view()),
-    url(r'^fabric/design/$', CollectionFabricDesignsList.as_view())
+    url(r'^fabric/$', views.CollectionFabricsList.as_view()),
+    url(r'^fabric/color/$', views.CollectionFabricColorsList.as_view()),
+    url(r'^fabric/design/$', views.CollectionFabricDesignsList.as_view())
 ]
 
 components = [
-    url(r'^collar/$', CollarTypeList.as_view()),
-    url(r'^cuff/$', CuffTypeList.as_view()),
-    url(r'^hem/$', HemTypeList.as_view()),
-    url(r'^back/$', BackTypeList.as_view()),
-    url(r'^pocket/$', PocketTypeList.as_view()),
-    url(r'^placket/$', PlacketTypeList.as_view()),
-    url(r'^sleeve/$', SleeveTypeList.as_view()),
+    url(r'^collar/$', views.CollarTypeList.as_view()),
+    url(r'^cuff/$', views.CuffTypeList.as_view()),
+    url(r'^hem/$', views.HemTypeList.as_view()),
+    url(r'^back/$', views.BackTypeList.as_view()),
+    url(r'^pocket/$', views.PocketTypeList.as_view()),
+    url(r'^placket/$', views.PlacketTypeList.as_view()),
+    url(r'^sleeve/$', views.SleeveTypeList.as_view()),
 ]
 
 urlpatterns = [
-    url(r'^collection/$', CollectionsListView.as_view()),
+    url(r'^collection/$', views.CollectionsListView.as_view()),
     url(r'^collection/(?P<pk>[0-9]+)/', include(collection_urls)),
     url(r'^components/', include(components)),
     url(r'^shirt/', include(shirt_urls)),
-    url(r'^shirt_info/', ShirtInfoListView.as_view()),
-    url(r'^size/$', SizesList.as_view()),
-    url(r'^size/option/$', SizeOptionsList.as_view()),
+    url(r'^shirt_info/', views.ShirtInfoListView.as_view()),
+    url(r'^size/$', views.SizesList.as_view()),
+    url(r'^size/option/$', views.SizeOptionsList.as_view()),
     url(r'^docs/', include('rest_framework_swagger.urls')),
 ]
