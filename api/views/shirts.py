8d9019c1ab215dd7d4267f12656271e3a5805de0
Arseniy Panfilov <cloud1567@yahoo.com>
2016-03-14 22:26:09 +0300

    COSSC-83 return objects instead of arrays

--- api/views/shirts.py
+++ api/views/shirts.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 import django_filters
 from django.conf import settings
-from rest_framework.generics import ListAPIView
-from rest_framework.generics import RetrieveAPIView
+from django.db.models import F
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework import pagination
 from rest_framework import filters
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
 from backend import models
 from dictionaries import models as dictionaries
 from api import serializers
@@ -63,9 +64,13 @@ class TemplateShirtsFiltersList(APIView):
         }
 
     def get(self, request, *args, **kwargs):
+        fabrics = models.Fabric.objects.filter(shirt__is_template=True, residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL)
+        colors = dictionaries.FabricColor.objects.filter(color_fabrics__shirt__is_template=True, color_fabrics__residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL)
+        designs = dictionaries.FabricDesign.objects.filter(design_fabrics__shirt__is_template=True, design_fabrics__residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL)
+        sex = [(x['sex'], models.SEX[x['sex']]) for x in models.Collection.objects.filter(shirts__is_template=True, shirts__fabric__residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL).values('sex').distinct()]
         return Response([
-            self.build_filter(u'Ткань', u'fabric', list(models.Fabric.objects.filter(shirt__is_template=True, residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL).values_list('id', 'code').distinct())),
-            self.build_filter(u'Цвет', 'fabric__colors', list(dictionaries.FabricColor.objects.filter(color_fabrics__shirt__is_template=True, color_fabrics__residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL).values_list('id', 'title').distinct())),
-            self.build_filter(u'Дизайн', 'fabric__designs', list(dictionaries.FabricDesign.objects.filter(design_fabrics__shirt__is_template=True, design_fabrics__residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL).values_list('id', 'title').distinct())),
-            self.build_filter(u'Пол', 'collection__sex', [(x['sex'], models.SEX[x['sex']]) for x in models.Collection.objects.filter(shirts__is_template=True, shirts__fabric__residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL).values('sex').distinct()]),
+            self.build_filter(u'Ткань', u'fabric', list(fabrics.annotate(title=F('code')).values('id', 'title').distinct())),
+            self.build_filter(u'Цвет', 'fabric__colors', list(colors.values('id', 'title', 'value').distinct())),
+            self.build_filter(u'Дизайн', 'fabric__designs', list(designs.values('id', 'title').distinct())),
+            self.build_filter(u'Пол', 'collection__sex', map(lambda pair: {'id': pair[0], 'title': pair[1]}, sex))
         ])
