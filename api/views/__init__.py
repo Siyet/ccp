746b053fb77323b116689f31cc2b624871e4049f
Gorkovenko Dmitry <gorkovenko.dmitry@gmail.com>
2016-03-19 12:58:39 +0800

    COSSC-68 add TuckOptionsList

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -4,6 +4,6 @@ from collections import CollectionFabricDesignsList, CollectionFabricsList, Coll
 from sizes import SizeOptionsList, SizesList
 
 from components import CollarTypeList, CuffTypeList, HemTypeList, BackTypeList, PocketTypeList, \
-    PlacketTypeList, SleeveTypeList, YokeTypeList, ShawlOptionsList
+    PlacketTypeList, SleeveTypeList, YokeTypeList, ShawlOptionsList, TuckOptionsList
 
 from shirts import TemplateShirtsList, TemplateShirtDetails, TemplateShirtsFiltersList
