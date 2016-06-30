17d28a42cb05c00f5c57915c54b408f9e10f7479
Arseniy Panfilov <cloud1567@yahoo.com>
2016-06-30 16:27:30 +0300

    add kassa settings for prod

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -5,3 +5,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['shirts.wecreateapps.ru']
 
 WSGI_APPLICATION = 'core.dev_wsgi.application'
+
+YANDEX_KASSA_DEBUG = True
+YANDEX_KASSA_SCID = 123
+YANDEX_KASSA_SHOP_ID = 123
+YANDEX_KASSA_SHOP_PASSWORD = 'password'
