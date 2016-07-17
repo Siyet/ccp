fb8b7cd61b0c8b81f509d6f76541c2c933c6245b
Arseniy Panfilov <cloud1567@yahoo.com>
2016-07-17 17:45:42 +0300

    missing migration

---
+++ backend/migrations/0065_auto_20160717_1712.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('backend', '0064_auto_20160717_0020'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='customshirt',
+            options={'verbose_name': '\u0420\u0443\u0431\u0430\u0448\u043a\u0430', 'verbose_name_plural': '\u0420\u0443\u0431\u0430\u0448\u043a\u0438'},
+        ),
+    ]
