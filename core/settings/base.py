b66c7d5b9d891f57c48ffa58659346b6d27d374a
root <root@localhost.localdomain>
2016-07-26 16:28:18 +0300

    remove gis dependency

--- core/settings/base.py
+++ core/settings/base.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
     'grappelli_orderable',
     'smart_selects',
     'imagekit',
