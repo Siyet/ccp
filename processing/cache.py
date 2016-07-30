fix compose scaling

processing/cache.py
@@ -20,7 +20,6 @@ L_FIELD = 'L'
 STITCHES = 'S'
 
 
-
 class CacheBuilder(object):
 
     SCALE_MAP = {
@@ -41,7 +40,7 @@ class CacheBuilder(object):
     }
 
     @staticmethod
-    def create_cache(instance, fields, field_types=None, scale=RENDER['preview_scale']):
+    def create_cache(instance, fields, field_types=None, cache_scale=RENDER['preview_scale']):
         matrices = []
         field_types = field_types or CacheBuilder.DEFAULT_FIELD_TYPES
 
@@ -60,11 +59,11 @@ class CacheBuilder(object):
 
                 array[..., 0] *= size[0]
                 array[..., 1] *= size[1]
-                array = ndimage.zoom(array, [scale, scale, 1], order=1)
+                array = ndimage.zoom(array, [cache_scale, cache_scale, 1], order=1)
 
             else:
                 img = image_from_array(array)
-                img = img.resize(scale_tuple(img.size, scale), Image.LANCZOS)
+                img = img.resize(scale_tuple(img.size, cache_scale), Image.LANCZOS)
                 array = np.asarray(img).astype('float32') / 255.0
 
             if isinstance(getattr(instance, 'content_object', None), BodyConfiguration):

processing/rendering/builder.py
@@ -336,7 +336,7 @@ class ShirtBuilder(object):
             return self.append_model(model)
         present_part_keys = filter(lambda k: k in part_keys, all_detail_keys)
         part_details = filter(lambda k: k['element'] in part_keys, self.contrast_details)
-
+        # CacheBuilder.create_cache(model, ('uv', 'light', 'ao'))
         uv = np.load(model.cache.get(source_field='uv').file.path)
         light_conf = model.cache.get(source_field='light')
         ao = model.cache.get(source_field='ao').file.path
@@ -349,6 +349,7 @@ class ShirtBuilder(object):
             for detail in part_details:
                 mask = conf.masks.filter(element=detail['element'], projection=self.projection).first()
                 if mask:
+                    # CacheBuilder.create_cache(mask, ('mask',))
                     detail_masks.append((detail, mask))
 
             if detail_masks: