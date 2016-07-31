6198cf26bd5919c45504253c861dc4dc94c0e753
Arseniy Panfilov <cloud1567@yahoo.com>
2016-07-31 15:45:18 +0300

    merged migrations

---
+++ backend/migrations/0069_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('backend', '0068_auto_20160730_1625'),
+        ('backend', '0068_auto_20160729_2205'),
+    ]
+
+    operations = [
+    ]
