9cd658c54320061782c22c91fe529c085db7a641
Arseniy Panfilov <cloud1567@yahoo.com>
2016-08-09 19:04:59 +0300

    dickey type images

--- api/views/components.py
+++ api/views/components.py
@@ -148,7 +148,7 @@ class DickeyList(FilterHelpersMixin, APIView):
         fabrics = models.Fabric.objects.active.filter(residuals__amount__gte=settings.MIN_FABRIC_RESIDUAL, dickey=True)
         fabrics_data = serializers.BaseFabricSerializer(fabrics.distinct(), many=True, context={'request': request}).data
         dickey_types = dictionaries.DickeyType.objects.all()
-        dickey_types_data = serializers.DickeyTypeSerializer(dickey_types, many=True).data
+        dickey_types_data = serializers.DickeyTypeSerializer(dickey_types, many=True, context={'request': request}).data
         return Response([
             self.build_filter(u'Ткань', 'fabric', fabrics_data),
             self.build_filter(u'Тип манишки', 'type', dickey_types_data),
