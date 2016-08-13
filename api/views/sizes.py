aa40a7210a6b9a2a317c348a6771e29caa31bcfd
Arseniy Panfilov <cloud1567@yahoo.com>
2016-08-13 16:44:08 +0300

    fix sizes endpoint

--- api/views/sizes.py
+++ api/views/sizes.py
@@ -5,8 +5,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.views.mixins import FilterHelpersMixin
-from backend.models import Shirt
-from dictionaries.models import Size, SizeOptions
+from dictionaries.models import Size, SizeOptions, Fit, SleeveLength
 from api import serializers
 
 
@@ -24,9 +23,10 @@ class SizesList(FilterHelpersMixin, APIView):
     """
 
     def get(self, request, *args, **kwargs):
+        fit_options = Fit.objects.values('id', 'title')
+        sleeve_options = SleeveLength.objects.values('id', 'title')
         return Response([
             self.build_filter(u'Размер', 'size', list(Size.objects.values('size', 'order'))),
-            self.build_filter(u'Талия', 'fit', map(lambda x: self.create_key_value_dict(*x), Shirt.FIT)),
-            self.build_filter(u'Длина рукава', 'sleeve_length',
-                              map(lambda x: self.create_key_value_dict(*x), Shirt.SLEEVE_LENGTH)),
+            self.build_filter(u'Талия', 'fit', fit_options),
+            self.build_filter(u'Длина рукава', 'sleeve_length', sleeve_options)
         ])
