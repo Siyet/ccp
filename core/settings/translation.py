c265056ef378060c8dd8b84080dd4392821f5ae0
Arseniy Panfilov <arseniy.panfilov@gmail.com>
2016-12-11 17:41:13 +0300

    api and core translations

--- core/settings/translation.py
+++ core/settings/translation.py
@@ -1,8 +1,18 @@
 # coding: utf-8
+import os
 
 LANGUAGES = [
     ('ru', u'Русский'),
     ('en', 'English'),
+]
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'api', 'locale'),
+    os.path.join(BASE_DIR, 'core', 'locale'),
+    os.path.join(BASE_DIR, 'backend', 'locale'),
+    os.path.join(BASE_DIR, 'checkout', 'locale'),
 ]
 
 LANGUAGE_CODE = 'ru'
