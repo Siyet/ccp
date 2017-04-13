935f7b8cc8871c9a8c03b3e57f3949924c874a5a
Arseniy Panfilov <cloud1567@yandex.ru>
2017-04-13 16:10:46 +0300

    Merged localization into master
    * cleanup
    
    * localization
    
    * compiled translations

--- api/views/components.py
+++ api/views/components.py
@@ -132,9 +132,9 @@ class TemplateInitialsList(APIView):
         colors = dictionaries.Color.objects.values('id', 'title', 'color')
         fonts = dictionaries.Font.objects.values('id', 'title', 'font')
         return Response([
-            self.build_filter(u'Цвет', 'color', list(colors)),
-            self.build_filter(u'Шрифт', 'font', map(lambda x: {'id': x['id'], 'title': x['title'], 'font': request.build_absolute_uri(x['font'])}, fonts)),
-            self.build_filter(u'Позиция', 'location', [{'key': x[0], 'value': unicode(x[1])} for x in models.Initials.LOCATION]),
+            self.build_filter(_(u'Цвет'), 'color', list(colors)),
+            self.build_filter(_(u'Шрифт'), 'font', map(lambda x: {'id': x['id'], 'title': x['title'], 'font': request.build_absolute_uri(x['font'])}, fonts)),
+            self.build_filter(_(u'Позиция'), 'location', [{'key': x[0], 'value': unicode(x[1])} for x in models.Initials.LOCATION]),
         ])
 
 
