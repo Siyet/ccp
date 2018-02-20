dc95c1956be8723705bcbf93e950da95a8bc3443
cctest <root@136723.simplecloud.club>
2018-02-20 05:54:36 +0000

    setup production logging

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -11,3 +11,24 @@ YANDEX_KASSA_DEBUG = False
 YANDEX_KASSA_SCID = 21869
 YANDEX_KASSA_SHOP_ID = 30291
 YANDEX_KASSA_SHOP_PASSWORD = 'XjKMW9ZADM1hFGu25iUf'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        # Log to a text file that can be rotated by logrotate
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': '/var/webapps/costumecode/logs/ccback-django.log'
+        },
+    },
+    'loggers': {
+        # Might as well log any errors anywhere else in Django
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
